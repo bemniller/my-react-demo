b05cebb0a178d2eb7778117613a3e61bf3d10769
Benjamin Miller <bm821002@gmail.com>
2023-10-01 19:44:45 -0400

    Updated to reflect most recent W3Schools tutorial

--- my-react-demo/src/index.js
+++ my-react-demo/src/index.js
@@ -3,9 +3,31 @@ import ReactDOM from 'react-dom/client';
 
 const myFirstElement = <h1>Hello React!</h1>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+const myelement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+);
+
+const combinedElement = (
+  <div>
+    {myFirstElement}
+    {myelement}
+  </div>
+);
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(combinedElement)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
